88e732091b182c3ff8b764e8855f6cc0088a3df0
awashbrook <awashbrook@gmail.com>
2016-03-05 14:04:30 +0000

    tests comparing array and lodash sorting

--- spec/lodash.spec.js
+++ spec/lodash.spec.js
@@ -18,11 +18,72 @@ describe("tests to learn lodash.js", function () {
     var deep = _.cloneDeep(objects);
     expect(deep[1]).toBe(deep[0]); // Can surprise, behaviour identical for angular.copy();
   });
-  it("shows _.sortBy function semantics are not like Array.sort with tuple args", function () {
-// Can sortBy really take two params like sort()?
-//    return _.sortBy(problems, function (a, b) {
-//      return extractPainFromCPI(b.cpi, cpiLabel) - extractPainFromCPI(a.cpi, cpiLabel);
-//    };
+  fdescribe("compare _.sortBy function semantics are not like Array.sort with tuple args", function () {
+    var fruit, sortedFruit, sortedValues, scores, stringyScores, things, ages, stringyAges;
+    beforeEach(function () {
+      fruit = ['cherries', 'apples', 'bananas'];
+      scores = [1, 10, 2, 21];
+      stringyScores = ['1', '10', '2', '21'];
+      things = ['word', 'Word', '1 Word', '2 Words'];
+      ages = [ { 'age': 1 }, { 'age': 10 }, { 'age': 2 }, { 'age': 21 } ];
+      stringyAges = [ { 'age': '1' }, { 'age': '10' }, { 'age': '2' }, { 'age': '21' } ];
+    });
+    it("array method obviously sort the original array of problems", function () {
+      sortedFruit = fruit.sort();
+      expect(sortedFruit).toEqual(['apples', 'bananas', 'cherries']);
+      expect(sortedFruit).toBe(fruit);
+    });
+    it("lodash will create a new array of problems", function () {
+      sortedFruit = _.sortBy(fruit);
+      expect(sortedFruit).toEqual(['apples', 'bananas', 'cherries']);
+      expect(sortedFruit).not.toBe(fruit);
+    });
+    it("array sort always alphanumeric, even when elements are integers: 1 > 10 > 2!", function () {
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+      expect(stringyScores.sort()).toEqual([ '1', '10', '2', '21' ]);
+      expect(scores.sort()).toEqual([1, 10, 2, 21]);
+      // Watch out that 10 comes before 2, because '10' comes before '2' in Unicode code point order.
+      var things = ['word', 'Word', '1 Word', '2 Words'];
+      expect(things.sort()).toEqual(['1 Word', '2 Words', 'Word', 'word']);
+      // In Unicode, numbers come before upper case letters, which come before lower case letters.
+    });
+    it("lodash respects strings & integers and does not attempt to convert them", function () {
+      expect(stringyScores.sort()).toEqual([ '1', '10', '2', '21' ]);
+      expect(_.sortBy(scores)).toEqual([ 1, 2, 10, 21 ]);
+      // Watch out that 10 comes before 2, because '10' comes before '2' in Unicode code point order.
+      expect(_.sortBy(things)).toEqual(['1 Word', '2 Words', 'Word', 'word']);
+      // In Unicode, numbers come before upper case letters, which come before lower case letters.
+    });
+    it("explicit comparitor needed to force a numeric array sort, coerces strings into integers and: 1 > 2 > 10", function () {
+      expect(scores.sort(function(a, b) {
+        return a - b;
+      })).toEqual([ 1, 2, 10, 21 ]);
+      expect(stringyScores.sort(function(a, b) {
+        return a - b;
+      })).toEqual([ '1', '2', '10', '21' ]);
+    });
+    it("lodash also needs coercing to force a strings into a numeric sort: 1 > 2 > 10", function () {
+      expect(_.sortBy(scores, function(s) {
+        return +s;
+      })).toEqual([ 1, 2, 10, 21 ]);
+      expect(_.sortBy(stringyScores, function(s) {
+        return +s;
+      })).toEqual([ '1', '2', '10', '21' ]);
+    });
+    it("lodash also needs coercing to force string object elements into a numeric sort: 1 > 2 > 10", function () {
+      sortedValues = _.map(
+        _.sortBy(ages, 'age'
+        ), _.values);
+      expect(sortedValues).toEqual( [ [ 1 ], [ 2 ], [ 10 ], [ 21 ] ]);
+      sortedValues = _.map(
+        _.sortBy(stringyAges, 'age'
+        ), _.values);
+      expect(sortedValues).toEqual([ [ '1' ], [ '10' ], [ '2' ], [ '21' ] ]);
+      sortedValues = _.map(
+        _.sortBy(stringyAges, function (o) { return +o.age; }
+        ), _.values);
+      expect(sortedValues).toEqual([ [ '1' ], [ '2' ], [ '10' ], [ '21' ] ]);
+    });
   });
   it("show how lodash sortByAll behaviour now part of sortAll", function () {
     var users = [
